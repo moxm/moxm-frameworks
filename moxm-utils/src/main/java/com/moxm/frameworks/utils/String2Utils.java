d11d6a3b59ce380846350ed5fe64ec5769678731
Richard <richard.ma@msn.com>
2020-11-28 20:08:39 +0800

    add utils

---
+++ moxm-utils/src/main/java/com/moxm/frameworks/utils/String2Utils.java
@@ -0,0 +1,154 @@
+package com.moxm.frameworks.utils;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * String Utils
+ * 
+ * @author <a href="http://www.moxm.com" target="_blank">Moxm</a> 2011-7-22
+ */
+public class StringUtils {
+
+    private StringUtils() {
+        throw new AssertionError();
+    }
+
+    /**
+     * is null or its length is 0 or it is made by space
+     * 
+     * <pre>
+     * isBlank(null) = true;
+     * isBlank(&quot;&quot;) = true;
+     * isBlank(&quot;  &quot;) = true;
+     * isBlank(&quot;a&quot;) = false;
+     * isBlank(&quot;a &quot;) = false;
+     * isBlank(&quot; a&quot;) = false;
+     * isBlank(&quot;a b&quot;) = false;
+     * </pre>
+     * 
+     * @param str
+     * @return if string is null or its size is 0 or it is made by space, return true, else return false.
+     */
+    public static boolean isBlank(String str) {
+        return (str == null || str.trim().length() == 0);
+    }
+
+    /**
+     * is null or its length is 0
+     * 
+     * <pre>
+     * isEmpty(null) = true;
+     * isEmpty(&quot;&quot;) = true;
+     * isEmpty(&quot;  &quot;) = false;
+     * </pre>
+     * 
+     * @param str
+     * @return if string is null or its size is 0, return true, else return false.
+     */
+    public static boolean isEmpty(CharSequence str) {
+        return (str == null || str.length() == 0);
+    }
+
+    /**
+     * compare two string
+     * 
+     * @param actual
+     * @param expected
+     * @return
+     * @see ObjectUtils#isEquals(Object, Object)
+     */
+    public static boolean isEquals(String actual, String expected) {
+        return ObjectUtils.isEquals(actual, expected);
+    }
+
+/**
+     * process special char in html
+     * 
+     * <pre>
+     * htmlEscapeCharsToString(null) = null;
+     * htmlEscapeCharsToString("") = "";
+     * htmlEscapeCharsToString("mp3") = "mp3";
+     * htmlEscapeCharsToString("mp3&lt;") = "mp3<";
+     * htmlEscapeCharsToString("mp3&gt;") = "mp3\>";
+     * htmlEscapeCharsToString("mp3&amp;mp4") = "mp3&mp4";
+     * htmlEscapeCharsToString("mp3&quot;mp4") = "mp3\"mp4";
+     * htmlEscapeCharsToString("mp3&lt;&gt;&amp;&quot;mp4") = "mp3\<\>&\"mp4";
+     * </pre>
+     * 
+     * @param source
+     * @return
+     */
+    public static String htmlEscapeCharsToString(String source) {
+        return StringUtils.isEmpty(source) ? source : source.replaceAll("&lt;", "<").replaceAll("&gt;", ">")
+                .replaceAll("&amp;", "&").replaceAll("&quot;", "\"");
+    }
+
+    /**
+     * transform half width char to full width char
+     * 
+     * <pre>
+     * fullWidthToHalfWidth(null) = null;
+     * fullWidthToHalfWidth("") = "";
+     * fullWidthToHalfWidth(new String(new char[] {12288})) = " ";
+     * fullWidthToHalfWidth("！＂＃＄％＆) = "!\"#$%&";
+     * </pre>
+     * 
+     * @param s
+     * @return
+     */
+    public static String fullWidthToHalfWidth(String s) {
+        if (isEmpty(s)) {
+            return s;
+        }
+
+        char[] source = s.toCharArray();
+        for (int i = 0; i < source.length; i++) {
+            if (source[i] == 12288) {
+                source[i] = ' ';
+                // } else if (source[i] == 12290) {
+                // source[i] = '.';
+            } else if (source[i] >= 65281 && source[i] <= 65374) {
+                source[i] = (char)(source[i] - 65248);
+            } else {
+                source[i] = source[i];
+            }
+        }
+        return new String(source);
+    }
+
+    /**
+     * transform full width char to half width char
+     * 
+     * <pre>
+     * halfWidthToFullWidth(null) = null;
+     * halfWidthToFullWidth("") = "";
+     * halfWidthToFullWidth(" ") = new String(new char[] {12288});
+     * halfWidthToFullWidth("!\"#$%&) = "！＂＃＄％＆";
+     * </pre>
+     * 
+     * @param s
+     * @return
+     */
+    public static String halfWidthToFullWidth(String s) {
+        if (isEmpty(s)) {
+            return s;
+        }
+
+        char[] source = s.toCharArray();
+        for (int i = 0; i < source.length; i++) {
+            if (source[i] == ' ') {
+                source[i] = (char)12288;
+                // } else if (source[i] == '.') {
+                // source[i] = (char)12290;
+            } else if (source[i] >= 33 && source[i] <= 126) {
+                source[i] = (char)(source[i] + 65248);
+            } else {
+                source[i] = source[i];
+            }
+        }
+        return new String(source);
+    }
+}
